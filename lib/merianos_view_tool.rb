a072fcb4b3b77bac6b629af03bd8a91294f6a312
I Mer <31615973+ioanmeri@users.noreply.github.com>
2018-03-25 20:36:02 +0300

    Dev tool initial commit

---
+++ lib/merianos_view_tool.rb
@@ -0,0 +1,6 @@
+require "merianos_view_tool/version"
+require "merianos_view_tool/renderer"
+
+module MerianosViewTool
+  # Your code goes here...
+end
---
+++ lib/merianos_view_tool/version.rb
@@ -0,0 +1,3 @@
+module MerianosViewTool
+  VERSION = "0.1.0"
+end
